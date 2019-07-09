26195eb2271d30b2157e18c91c3ce4abbbb91cc9
gatosempai <gato_2224@hotmail.com>
2019-07-09 10:23:12 -0500

    exercise 1 and 2 completed

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+    NegativeNumberValidator negativeNumberValidator;
+
+    @Before
+    public void setUp() {
+        negativeNumberValidator = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void minusNumber() {
+        boolean result = negativeNumberValidator.isNegative(-1);
+        Assert.assertThat(result, is(true));
+    }
+
+    @Test
+    public void zeroNumber() {
+        boolean result = negativeNumberValidator.isNegative(0);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void plusNumber() {
+        boolean result = negativeNumberValidator.isNegative(1);
+        Assert.assertThat(result, is(false));
+    }
 
 }
--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,34 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringDuplicatorTest {
 
+    StringDuplicator stringDuplicator;
+
+    @Before
+    public void setUp() throws Exception {
+        stringDuplicator = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicator_emptyString_returned() {
+        String result = stringDuplicator.duplicate("");
+        Assert.assertThat(result, CoreMatchers.is(""));
+    }
+
+    @Test
+    public void duplicator_singleString_returned() {
+        String result = stringDuplicator.duplicate("a");
+        Assert.assertThat(result, CoreMatchers.is("aa"));
+    }
+
+    @Test
+    public void duplicator_longString_returned() {
+        String result = stringDuplicator.duplicate("hello");
+        Assert.assertThat(result, CoreMatchers.is("hellohello"));
+    }
 }
