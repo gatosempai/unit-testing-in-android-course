e302c8bfeb63544970826235a5b2a8ac9bc68e2d
gatosempai <gato_2224@hotmail.com>
2019-07-10 15:29:26 -0500

    Test for exercise 5 added

--- mockito_fundamentals/src/test/java/com/techyourchance/mockitofundamentals/exercise5/UpdateUsernameUseCaseSyncTest.java
+++ mockito_fundamentals/src/test/java/com/techyourchance/mockitofundamentals/exercise5/UpdateUsernameUseCaseSyncTest.java
@@ -1,5 +1,39 @@
 package com.techyourchance.mockitofundamentals.exercise5;
 
+import com.techyourchance.mockitofundamentals.exercise5.eventbus.EventBusPoster;
+import com.techyourchance.mockitofundamentals.exercise5.networking.NetworkErrorException;
+import com.techyourchance.mockitofundamentals.exercise5.networking.UpdateUsernameHttpEndpointSync;
+import com.techyourchance.mockitofundamentals.exercise5.users.UsersCache;
+
+import org.junit.Before;
+
+import static com.techyourchance.mockitofundamentals.exercise5.networking.UpdateUsernameHttpEndpointSync.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class UpdateUsernameUseCaseSyncTest {
 
+    public static final String USER_ID = "userId";
+    public static final String USER_NAME = "userName";
+    UpdateUsernameUseCaseSync SUT;
+    UpdateUsernameHttpEndpointSync updateUsernameHttpEndpointSyncMock;
+    UsersCache usersCacheMock;
+    EventBusPoster eventBusPosterMock;
+
+    @Before
+    public void setUp() throws Exception {
+        updateUsernameHttpEndpointSyncMock = mock(UpdateUsernameHttpEndpointSync.class);
+        usersCacheMock = mock(UsersCache.class);
+        eventBusPosterMock = mock(EventBusPoster.class);
+        SUT = new UpdateUsernameUseCaseSync(updateUsernameHttpEndpointSyncMock, usersCacheMock, eventBusPosterMock);
+        success();
+    }
+
+    // TODO continue whit tests
+
+    private void success() throws NetworkErrorException {
+        when(updateUsernameHttpEndpointSyncMock.updateUsername(any(String.class), any(String.class)))
+                .thenReturn(new EndpointResult(EndpointResultStatus.SUCCESS, USER_ID, USER_NAME));
+    }
 }
